3df40a524cf2167b771fb1dc8ab47226f8e668fd
Psidra <xdusk37@gmail.com>
2017-08-28 10:12:50 +0800

    added offset AGAIN

--- NYP_Framework/Base/Source/UIElement.cpp
+++ NYP_Framework/Base/Source/UIElement.cpp
@@ -150,7 +150,10 @@ void UIElement::Update()
 
 	if (this->m_bCollider)
 	{
-		this->SetAABB(Vector3((this->scale * 0.5f) + this->position), Vector3((this->scale * -0.5f) + this->position));
+		Vector3 offset = this->position;
+		offset.y += 15.f;
+
+		this->SetAABB(Vector3((this->scale * 0.5f) + offset), Vector3((this->scale * -0.5f) + offset));
 	}
 
 	if (this->type == CURSOR)
